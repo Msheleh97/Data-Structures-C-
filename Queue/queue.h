412fb17a247e517502ca8bb596adfe21c217abb1
Mohammad Mshaleh <88254919+Msheleh97@users.noreply.github.com>
2022-03-02 19:28:26 +0100

    Update queue.h

--- Queue/queue.h
+++ Queue/queue.h
@@ -2,7 +2,7 @@
  * Queue
  *
  * @author  Mohammad Mshaleh
- * @since   2021-01-02
+ * @since   2019-12-02
  *
  * @{
  */
@@ -14,8 +14,7 @@
 #include "list.h"
 
 // The type for the queue.
-typedef struct queue
-{
+typedef struct queue{
     List *list;
 } Queue;
 
@@ -42,37 +41,35 @@ typedef struct queue
   */void queue_destroy(Queue *q);
 
 
+ /**
+  * name : queue_enqueue.
+  * @brief Returns nothing.
+  *  Add a value to the tail of the queue.
+  *  The value is copied to dynamically allocated memory.
+  *
+  * @param pointer to a queue.
+  * @return void.
+  */void queue_enqueue(Queue *q, const char *value);
+
 
   /**
-   * name : queue_enqueue.
-   * @brief Returns nothing.
-   *  Add a value to the tail of the queue.
-   *  The value is copied to dynamically allocated memory.
+   * name : queue_dequeue.
+   * @brief Returns a pointer to a char.
+   * Remove the value at the head of the queue.
    *
-   * @param pointer to a queue.
-   * @return void.
-   */void queue_enqueue(Queue *q, const char *value);
+   *
+   * @param a pointer to the queue.
+   * @return Returns a pointer to char.
+   */char *queue_dequeue(Queue *q);
 
-
-   /**
-    * name : queue_dequeue.
-    * @brief Returns a pointer to a char.
-    * Remove the value at the head of the queue.
-    *
-    *
-    * @param a pointer to the queue.
-    * @return Returns a pointer to char.
-    */char *queue_dequeue(Queue *q);
-
-    /**
-     * name : queue_is_empty
-     * @brief Returns a bool.
-     * Check if the queue is empty.
-     *
-     *
-     * @param a pointer to a constant char.
-     * @return Returns true/false.
-     */bool queue_is_empty(const Queue *q);
-
+  /**
+   * name : queue_is_empty
+   * @brief Returns a bool.
+   * Check if the queue is empty.
+   *
+   *
+   * @param a pointer to a constant char.
+   * @return Returns true/false.
+   */bool queue_is_empty(const Queue *q);
 
 #endif /* QUEUE_H */
